f7be83b1aa6ed399423b12bfe69cd81cd6cab26f
awildtechno <ajdweavers@gmail.com>
2019-06-26 15:29:59 -0700

    Updated the GameLoop class a bit more.

--- gameloop/GameLoop.py
+++ gameloop/GameLoop.py
@@ -1,24 +1,34 @@
 import pygame
+from OpenGL.GL import *
+from OpenGL.GLU import *
 
 
 class GameLoop:
     def __init__(self, drawables):
         self.exit_flag = False
-        if not (hasattr(drawables, '__iter__') or hasattr(drawables, '__getitem__')):
-            raise TypeError("drawables must be an iterable.")
+        if not isinstance(drawables, set):
+            raise TypeError("drawables should be a Set. Sets provide \
+                fast lookup and avoid duplicates")
+
         self.drawables = drawables
         self._event_handlers = {}
         self.state = {}  # a dictionary for storing in-game variables.
+        self.exit_flag = False  # a flag to exit the game.
 
     def define_handler(self, pygame_event, handler):
         '''
-        A nice semantic way to add event handler functions to the game.
-        An example of MOUSEBUTTONDOWN handler.
+        A semantic way to add event handler functions to the game loop.
+
+        :param pygame_event: The event object from PyGame.
+        :param handler: A function that handles the event with the format
+          def <name>(loop, event)
+
+        An example of MOUSEBUTTONDOWN handler:
         def handle_click(loop, event):
             # loop: the event loop will pass itself in.
             # event: the event object from pygame
             if button == 1:  # left click
-                loop.state['MOUSEDOWN'] = true  # set the loop state 
+                loop.state['MOUSEDOWN'] = true  # set the loop state
                 loop.drawables.append(potato)   # show a potato
 
         Common pygame events and their params:
@@ -37,13 +47,21 @@ class GameLoop:
             VIDEOEXPOSE      none
             USEREVENT        code
 
-            
-        Note: pygame.QUIT is not overridable to avoid jerks.
+        Note: pygame.QUIT is not overridable in the default loop to avoid jerks.
         '''
         self._event_handlers[pygame_event] = handler
 
     def begin(self, display, clock, clock_rate):
         "Begins the game loop on the given display."
+
+        # OpenGL setup.
+        # Set perspective, aspect ratio, clipping bounds
+        gluPerspective(45, (display.get_width() / display.get_height()), 0.2, 10.)
+        # Zoom out a bit.
+        glTranslatef(0., 0., -1.)  # zoom out a bit
+        # Rotate the entire field a bit to simulate a camera angle.
+        glRotatef(22, 0, 0, 0)
+
         self.exit_flag = False
         while not self.exit_flag:
             for event in pygame.event.get():
@@ -54,25 +72,24 @@ class GameLoop:
                 if event.type in self._event_handlers:
                     self._event_handlers[event.type](self, event)
 
-            glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT) # clear the screen
-            for d in drawables:
+            # clear the screen
+            glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
+            for d in self.drawables:
                 if (hasattr(d, 'GLDraw')):
                     d.GLDraw()
-                elif (hasattr(d, 'surface')):
-                    display.blit(d.surface, d.pos)
                 else:
-                    raise TypeError("Drawable " + repr(d) + " can't be drawn. Please add a .survace or .draw() method.")
-                if (hasattr(d, 'move')):
-                    d.move()
+                    raise AttributeError('GameLoop: drawable has no .GLDraw() \
+                        and cannot be drawn.')
 
         pygame.display.flip()
         clock.tick(clock_rate)
 
-    def __enter__(self):  # A good place to load game assets. Override as needed.
+    def __enter__(self):  # A place to load game assets. Override as needed.
         "Actions performed when the class is created using a 'with' statement."
-        pass
+        print("Game loop created.")
+        return self
 
     def __exit__(self, type, value, traceback):  # Destroy game assets. Override as needed.
-        "Actions performed when the loop exits."
-        if (type):
-            raise type(value)  # Reraise the error.
+        "Actions performed when the class exits."
+        print("Exiting game loop.")
+        return False  # do not supress any errors.
--- pingpong.py
+++ pingpong.py
@@ -1,11 +1,10 @@
 import pygame
-from OpenGL.GL import *
-from OpenGL.GLU import *
 import numpy as np
 # from pingpong.game_objects.testobjs import player, table, net, ball
 from gl.drawable import PingPongBall, Point3D, Shape3D
 from gl.pallette import C_WHITE
 from gl.constants import WIDTH, HEIGHT
+from gameloop.GameLoop import GameLoop
 
 # Game Variables
 GAME_PACE_SCALAR = 1.
@@ -16,41 +15,17 @@ gameDisplay = pygame.display.set_mode((WIDTH, HEIGHT), pygame.DOUBLEBUF|pygame.O
 pygame.display.set_caption('Ping Pong')
 clock = pygame.time.Clock()
 
-# OpenGL Init
-gluPerspective(45, (WIDTH / HEIGHT), 0.2, 50.)
-glTranslatef(0., 0., -10.)
-glRotatef(0, 0, 0, 0)
-black = (0, 0, 0)
-
 # Ordered!
 # drawables = [ball, net, player, table]
-drawables = [Shape3D([Point3D(0.1, 0.1, .5), 
+drawables = {Shape3D([Point3D(0.1, 0.1, .5), 
                       Point3D(0.3, 0.1, .5),
                       Point3D(0.3, 0.2, .5),
                       Point3D(0.1, 0.2, .5)], 
                       (120,120,15)),
-             Point3D(0.2, 0.2, .5)]
+             Point3D(0.2, 0.2, .5)}
 
-exit_flag = False
-
-while not exit_flag:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            break
-
-    glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)  # clear the screen
-    for d in drawables:
-        if (hasattr(d, 'GLDraw')):
-            d.GLDraw()
-        elif (hasattr(d, 'surface')):
-            gameDisplay.blit(d.surface, d.pos)
-        else:
-            raise TypeError("Drawable " + repr(d) + " can't be drawn. Please add a .survace or .draw() method.")
-        if (hasattr(d, 'move')):
-            d.move()
-
-    pygame.display.flip()
-    clock.tick(60)
+with GameLoop(drawables) as loop:
+    loop.begin(gameDisplay, clock, 60)
 
 pygame.quit()
 quit()
